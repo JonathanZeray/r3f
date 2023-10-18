e17eb7217bf6e2dd4c0f952d20663b9de3351d44
Jonathan Zeray <jonathan.zeray@hotmail.com>
2023-10-18 16:28:37 +0200

    Complete

---
+++ src/components/Office.jsx
@@ -0,0 +1,113 @@
+import React, { useLayoutEffect, useRef } from 'react'
+import { useGLTF, useScroll } from '@react-three/drei'
+import gsap from 'gsap';
+import { useFrame } from '@react-three/fiber';
+
+export const FLOOR_HEIGHT = 2.3;
+export const NB_FLOORS = 3;
+
+export function Office(props) {
+  const { nodes, materials } = useGLTF('/models/WawaOffice.glb')
+  const ref = useRef();
+  const tl = useRef();
+  const libraryRef = useRef();
+  const atticRef = useRef();
+
+  const scroll = useScroll();
+
+  useFrame(() => {
+    tl.current.seek(scroll.offset * tl.current.duration());
+  });
+  
+  useLayoutEffect(() => {
+    tl.current = gsap.timeline();
+
+    // Vertical animation
+
+    tl.current.to(
+        ref.current.position,
+        {
+            duration: 2,
+            y: -FLOOR_HEIGHT * (NB_FLOORS - 1),
+        },
+        0
+    );
+
+    //LIBRARY REF 
+
+    tl.current.from(
+        libraryRef.current.position,
+        {
+            duration: 2,
+            x: -2,
+        },
+        0.5
+    );
+    tl.current.from(
+        libraryRef.current.rotation,
+        {
+            duration: 3,
+            y: -Math.PI / 2,
+        },
+        0
+    );
+
+    //ATTIF REF
+
+    tl.current.from(
+        atticRef.current.position,
+        {
+            duration: 3,
+            y: 1,
+        },
+        0,
+    );
+    tl.current.from(
+        atticRef.current.position,
+        {
+            duration: 3,
+            z: -1,
+        },
+        1.5
+    )
+
+    tl.current.from(
+        atticRef.current.rotation,
+        {
+            duration: 4,
+            y: -Math.PI / 2,
+        },
+        0
+    );
+
+  }, []);
+
+ 
+ 
+ 
+  return (
+    <group {...props} dispose={null} ref={ref}>
+      <mesh geometry={nodes['01_office'].geometry} material={materials['01']} />
+      <group
+       position={[0, 2.11, -2.23]}>
+        <group ref={libraryRef}>
+      <mesh
+       geometry={nodes['02_library'].geometry} 
+       material={materials['02']}  
+       />
+      </group>
+      </group>
+      <group position={[-1.97, 4.23, -2.2]}>
+        <group ref={atticRef}>
+      <mesh 
+      geometry={nodes['03_attic'].geometry} 
+      material={materials['03']} 
+      />
+
+    </group>
+    </group>
+    </group>
+  )
+}
+
+useGLTF.preload('/models/WawaOffice.glb')
